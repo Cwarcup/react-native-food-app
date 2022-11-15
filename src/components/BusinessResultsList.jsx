abec262c9b1d33c9ffcf57b919a548793b701e0e
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-11-14 17:48:23 -0800

    individual business

--- src/components/BusinessResultsList.jsx
+++ src/components/BusinessResultsList.jsx
@@ -4,6 +4,10 @@ import ResultsDetail from "./ResultsDetail"
 import { withNavigation } from "react-navigation"
 
 const BusinessResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
@@ -15,7 +19,9 @@ const BusinessResultsList = ({ title, results, navigation }) => {
         renderItem={({ item }) => {
           return (
             <TouchableOpacity
-              onPress={() => navigation.navigate("ResultsShow")}
+              onPress={() =>
+                navigation.navigate("ResultsShow", { id: item.id })
+              }
             >
               <ResultsDetail result={item} />
             </TouchableOpacity>
--- src/screens/ResultsShowScreen.jsx
+++ src/screens/ResultsShowScreen.jsx
@@ -1,13 +1,53 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, FlatList, Image } from "react-native"
+import { useState, useEffect } from "react"
+import yelp from "../api/yelp"
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null)
+
+  const id = navigation.getParam("id")
+  console.log({ id })
+
+  // get the business details from the API
+  const getBusinessDetails = async (id) => {
+    const response = await yelp.get(`/${id}`)
+    setResult(response.data)
+  }
+
+  useEffect(() => {
+    getBusinessDetails(id)
+  }, [])
+
+  if (!result) {
+    return null
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        horizontal={true}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />
+        }}
+      />
+      <Text style={styles.subtext}>{result.location?.display_address[0]} </Text>
+      <Text style={styles.subtext}>{result.phone}</Text>
     </View>
   )
 }
 
 export default ResultsShowScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+  subtext: {
+    fontSize: 14,
+    color: "grey",
+  },
+})
